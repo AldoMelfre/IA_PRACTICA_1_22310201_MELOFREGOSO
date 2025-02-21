911729a982536c461689ecb469490d3199fbf501
Aldo Carlo Melo Fregoso <a22310201@ceti.mx>
2025-02-21 12:17:46 -0600

    Create _002_Literales.py

---
+++ _002_Literales.py
@@ -0,0 +1,34 @@
+#Un literal se refiere a datos cuyos valores están determinados por el literal mismo.
+#Los literales son constantes que representan valores fijos.
+#En Python, los literales se pueden clasificar en los siguientes tipos:
+#Literales de cadena que ya conocemos
+
+#Literales de cadena con caracter de escape \  (backslash), con esto no generamos errores al imprimir comillas dobles o simples en una cadena de texto.
+print("Me gusta \"Monty Python\"")
+
+print('\n---------------------\n')
+
+#Literales enteros
+
+print("2")
+print(2)
+
+
+print('\n---------------------\n')
+
+#Literales de punto flotante, Cuando se usan términos como dos y medio o menos cero punto cuatro, pensamos en números que la computadora considera como números punto-flotante:
+
+#El punto decimal es lo que determina si es flotante.
+
+print(2) #Entero
+print(2.0) #Flotante
+
+print('\n---------------------\n')
+
+#Por ejemplo, la velocidad de la luz, expresada en metros por segundo. Escrita directamente se vería de la siguiente manera: 300000000. Para evitar escribir tantos ceros, los libros de texto emplean la forma abreviada, la cual probablemente hayas visto: 3 x 108
+
+print(300000000) #Entero
+print(3e8) #Flotante
+
+print('\n---------------------\n')
+
