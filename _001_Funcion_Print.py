6b95a6bcb0a78b02a02c7c352e2a8e170830edc1
Aldo Carlo Melo Fregoso <a22310201@ceti.mx>
2025-02-21 11:31:27 -0600

    Create _001_Funcion_Print.py

---
+++ _001_Funcion_Print.py
@@ -0,0 +1,67 @@
+#IMPRIMIR UN MENSAJE EN PANTALLA
+#Ejemplo de un programa que imprime un mensaje en pantalla
+print("¡Hola, Mundo!")
+
+
+print('\n---------------------\n')
+#Imprimir mi nombre
+print('Aldo Carlo Melo Fregoso')
+
+
+print('\n---------------------\n')
+#Imprimir mi nombre sin comillas (error)
+#print(Aldo Carlo Melo Fregoso)
+#print"Aldo Carlo Melo Fregoso"
+
+
+print('\n---------------------\n')
+#Imprimir mi nombre con comillas dobles
+print("Aldo Carlo Melo Fregoso")
+
+
+print('\n---------------------\n')
+#Ejemplo de un programa que imprime un mensaje expandido en pantalla
+print("La Witsi Witsi Araña subió a su telaraña.")
+print("Vino la lluvia y se la llevó.")
+
+
+print('\n---------------------\n')
+#Ejemplo de un programa que imprime un mensaje con salto de linea en pantalla
+print("La Witsi Witsi Araña subió a su telaraña.")
+print()
+print("Vino la lluvia y se la llevó.")
+
+
+print('\n---------------------\n')
+#Ejemplo de un programa que imprime un mensaje con salto de linea con la funcion \n en pantalla
+print("La Witsi Witsi Araña\nsubió a su telaraña.")
+print()
+print("Vino la lluvia\ny se la llevó.")
+
+
+print('\n---------------------\n')
+#Ejemplo de un programa que imprime un mensaje concatenado o en cadena
+print("La Witsi Witsi Araña" , "subió" , "a su telaraña.")
+
+
+print('\n---------------------\n')
+#Ejemplo de mensaje con argumento end
+print("Mi nombre es", "Aldo.", end=" ")
+print("Aldo Fregoso.")
+
+
+print('\n---------------------\n')
+#Ejemplo de mensaje con argumento sep
+print("Mi", "nombre", "es", "Aldo", "Fregoso.", sep="-")
+
+
+print('\n---------------------\n')
+#Ejemplo de mensaje con argumento sep y end
+print("Programming","Essentials","in", sep="***", end="...")
+print("Python")
+
+
+
+
+
+
