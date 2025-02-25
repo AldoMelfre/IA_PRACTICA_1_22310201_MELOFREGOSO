f21401af146cf3de958481f6cdf9815f06428842
Aldo Carlo Melo Fregoso <a22310201@ceti.mx>
2025-02-24 22:51:12 -0600

    Update _005_Comentarios.py

--- _005_Comentarios.py
+++ _005_Comentarios.py
@@ -21,7 +21,7 @@ print(x + y)
 # uncomment_me_too = 3
 # uncomment_me_also = 5
 
-print(uncomment_me, uncomment_me_too, uncomment_me_also, sep="\n")
+#print(uncomment_me, uncomment_me_too, uncomment_me_also, sep="\n")
 
 #este programa calcula los segundos en cierto número de horas determinadas
 # este programa fue escrito hace dos días
