a45f73e40cd11441ec03fc8a55df56e59f407d12
Aldo Carlo Melo Fregoso <a22310201@ceti.mx>
2025-03-09 14:13:13 -0600

    Operador NOt

---
+++ _022_Operador_Not.py
@@ -0,0 +1,15 @@
+#EL operador NOT regresa el valor contrario booleano de la expresion que pasa como argumento, por un decir, si una expresion resulta verdadera, el operador not la convierte en falsa y viceversa.
+#
+#Ejemplo de uso de NOT
+x = 15
+10>x<20
+print(not 10>20) #TRUE
+print(not 10<20) #FALSE
+
+#Ejemplo de uso de NOT
+x = 15
+y = 10
+x = 15
+print(not x==y) #TRUE, en escencia es false la respuesta pero el NOT invierte el valor
+print(not x==x) #FALSE, en escencia es true la respuesta pero el NOT invierte el valor
+
