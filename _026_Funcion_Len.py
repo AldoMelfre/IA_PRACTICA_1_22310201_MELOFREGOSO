02a25ee3d8aaf11f442996b3f64d21867dbc76ea
Aldo Carlo Melo Fregoso <a22310201@ceti.mx>
2025-03-11 22:50:30 -0600

    Que hace la funcion len

---
+++ _026_Funcion_Len.py
@@ -0,0 +1,22 @@
+#La funcion len() devuelve la longitud de un objeto, como una lista, una cadena, un diccionario, etc.
+#Por ejemplo, para obtener la longitud de una lista, podemos hacer lo siguiente:
+numbers = [1, 2, 3, 4, 5]
+print(len(numbers)) #5
+
+# La función len() devuelve la longitud de un objeto, como una lista, una cadena, un diccionario, etc.
+# A continuación, se presentan varios ejemplos de cómo se puede utilizar la función len().
+# Ejemplo con una lista
+numbers = [1, 2, 3, 4, 5]
+print(len(numbers))  # 5
+# Ejemplo con una cadena de texto (string)
+texto = "Hola, mundo"
+print(len(texto))  # 11
+# Ejemplo con una tupla
+tupla = (1, 2, 3, 4, 5)
+print(len(tupla))  # 5
+# Ejemplo con un diccionario
+diccionario = {'a': 1, 'b': 2, 'c': 3}
+print(len(diccionario))  # 3
+
+# La función len() es muy versátil y se puede aplicar a cualquier objeto que implemente el método especial __len__().
+
